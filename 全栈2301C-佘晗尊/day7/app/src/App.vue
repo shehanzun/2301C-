04557cce7abc1c162edac1ff1a6ee70679e69f07
佘晗尊 <149566784+shehanzun@users.noreply.github.com>
2023-11-02 12:00:47 +0800

    Add files via upload
    作业

---
+++ 全栈2301C-佘晗尊/day7/app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+
+
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+li {
+  list-style: none;
+}
+::v-deep .van-tabbar {
+  background-color: black;
+}
+</style>
+
+
---
+++ 全栈2301C-佘晗尊/day7/app/src/components/yuan.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+    <div id="yuan" v-show="checked=='true'" @click="fal">
+      <img src="@/icon/yue.png" alt="">
+  </div>
+
+  <div id="yuan1" v-show="checked=='false'" @click="tru">
+      <img src="@/icon/yue1.png" alt="">
+  </div>
+</div>
+</template>
+<script setup lang='ts'>
+const checked=ref('true')
+localStorage.setItem('state',JSON.stringify(checked.value))
+import { ref } from "vue";
+
+const fal=()=>{
+    checked.value='false'
+    localStorage.setItem('state',JSON.stringify(checked.value))
+}
+
+const tru=()=>{
+    checked.value="true"
+    localStorage.setItem('state',JSON.stringify(checked.value))
+}
+</script>
+<style scoped lang='scss'>
+#yuan{
+    position: fixed;
+    right: 30px;
+    bottom: 150px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    box-shadow: 0px 0px 5px 2px #ccc;
+    img{
+      width: 20px;
+      height: 20px;
+
+    }
+}
+
+#yuan1{
+    position: fixed;
+    right: 30px;
+    bottom: 150px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #63a48e;
+    box-shadow: 0px 0px 5px 2px #ccc;
+    img{
+      width: 20px;
+      height: 20px;
+
+    }
+}
+</style>
